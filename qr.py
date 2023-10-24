21d77ae7a058c6b5ce089f19c62783085566041f
Abdulkadir Kızılarslan <132955266+Akdrkzl@users.noreply.github.com>
2023-10-24 09:40:27 +0300

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,8 @@
+# terminale pip install qrcode pillow  
+import qrcode
+
+# Daha sonra code adında bir değişken oluşturalım ve qrcode.make('') komutunu kullanalım. Burada isterseniz bir metin girişi yapabilirsiniz veya isterseniz bir url girişi yapabilirsiniz.
+code =  qrcode.make('https://github.com/Akdrkzl')
+code.save('qrcode.png')
+# save metodu ile çıktımızı alıyoru.Burada save içerisine hangi formatta çıktı almak istediğimizi ve çıktı alırken hangi isimde olması gerektiğini belirliyoruz.
+# son olarak terminalde python qr.py çalıştırıp çıktımızı alıyoruza
